290eb85c850c984ab326e383924199a73a13e4eb
Dyoung <wkftkfwk0118@gmail.com>
2022-10-21 18:12:12 +0900

    제주맥주1021퍼블리싱중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 window.onload = function(){
-  
+  var swiper = new Swiper(".logomenuswiper", {
+    slidesPerView: 6,
+    spaceBetween: 15,
+    freeMode: false,
+    pagination: {
+      clickable: true,
+    },
+    autoplay: {
+      delay: "500",
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    loop: true,
+    speed: 20000,
+  });
 }
